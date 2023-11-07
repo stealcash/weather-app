81e69d7e62091252b2dd4acc440bb394f9cff13a
stealcash <pawansaini312@gmail.com>
2023-11-08 03:38:10 +0530

    change config file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const WEATHER_API_BASE_URL = "https://api.weatherapi.com/v1";
 export const WEATHER_API_KEY = "84df56405f124128a93173027230611";
-export const SITE_BASE_PATH = "/dev/";
+export const SITE_BASE_PATH = "";
